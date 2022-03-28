023b368fa9a7cba21f4d2eef0af1d05ce51196cc
Tim Simms <tsimms@gmail.com>
2022-03-28 14:43:10 -0500

    testing

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,12 +3,6 @@
 from flask import jsonify, Flask
 from api.v1.views import app_views
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
@@ -17,28 +11,12 @@ def status():
     return jsonify({"status": "OK"})
 
 
-# @app_views.route('/stats', methods=['GET'])
-# def stats():
-#     """return number of stats"""
-#     return jsonify({"amenities": storage.count("Amenity"),
-#                     "cities": storage.count("City"),
-#                     "places": storage.count("Place"),
-#                     "reviews": storage.count("Review"),
-#                     "states": storage.count("State"),
-#                     "users": storage.count("User")})
-
-@app_views.route('/stats', strict_slashes=False, methods=['GET'])
+@app_views.route('/stats', methods=['GET'])
 def stats():
-    """ returns dictionary with classes and their count"""
-    classes = {
-        "amenities": Amenity,
-        "cities": City,
-        "places": Place,
-        "reviews": Review,
-        "states": State,
-        "users": User
-    }
-    countDict = {}
-    for objType, cls in classes.items():
-        countDict.update({objType: storage.count(cls)})
-    return jsonify(countDict)
+    """return number of stats"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
