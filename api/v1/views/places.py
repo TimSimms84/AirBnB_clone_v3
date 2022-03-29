5c228e09d4983fdf48a8439ad9e04d54b23bf9e7
Tim Simms <tsimms@gmail.com>
2022-03-29 15:54:49 -0500

    pep8 changes

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -51,7 +51,8 @@ def place_delete(place_id):
     return jsonify({}), 200
 
 
-@app_views.route("/cities/<city_id>/places", methods=['POST'], strict_slashes=False)
+@app_views.route("/cities/<city_id>/places", methods=['POST'],
+                 strict_slashes=False)
 def post_place(city_id):
     """add place using POST"""
     city = storage.get(City, city_id)
